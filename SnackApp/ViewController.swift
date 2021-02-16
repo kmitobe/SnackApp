81d146071dddbf9c8779c910e0ffe0316d7ece8d
Kou <Kou@Kou.local>
2021-02-17 08:35:52 +0900

    jsonでのデータ取得まで完了

--- SnackApp/ViewController.swift
+++ SnackApp/ViewController.swift
@@ -33,6 +33,11 @@ class ViewController: UIViewController ,UISearchBarDelegate{
         
         
     }
+    //お菓子の配列中にタプルを用意（オプショナル）
+    //タプルは要素の中身を変更できない
+    //タプルを配列で持っておけばレコードごとに格納できる
+    var okashiList:[(name:String,maker:String,link:URL,image:URL)]? = []
+    
     //searchOkashiメソッド
     //第一引数：keyword　検索したいワード
     func searchOkashi(keyword:String){
@@ -45,8 +50,70 @@ class ViewController: UIViewController ,UISearchBarDelegate{
         guard let req_url = URL(string: "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
             return
         }
-        
+        //リクエストのURLをデバックエリアに出力
         print(req_url)
+        //リクエストの処理
+        //リクエストに必要な情報を生成(リクエストURLを使ってリクエストオブジェクトを生成)
+        let req = URLRequest(url: req_url)
+        //リクエストURLからリクエストを管理する為の、req（リクエストオブジェクトを生成しています）
+        //第一引数：セッション構成 デフォルト
+        //第二引数：デリゲート先 今回はクロージャで行う為nil
+        //第三引数：デリゲートキューの設定
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        //リクエストをタスクとして登録
+        //第一引数：リクエストを管理するオブジェクト
+        //第二引数：クロージャ ダウンロードが完了するとcompletionHandlerが実行される
+        let task = session.dataTask(with: req, completionHandler:{(data,response,error)in
+            //セッションを終了
+            session.finishTasksAndInvalidate()
+            //do try catchエラーハンドリング
+            do{
+                //JSONDecoderインスタンス
+                let decoder = JSONDecoder()
+                let json = try decoder.decode(ResultJson.self,from: data!)
+                //print(json)
+                //jsonが取得できているかを確認
+                if let items = json.item{
+                        //取得したアイテム分ループする
+                        for item in items{
+                            //アイテムを各変数にアンラップする
+                            if let name = item.name,let maker = item.maker,let link = item.url,let image = item.image {
+                                //タプル配列に格納する
+                                let okashi = (name ,maker ,link ,image)
+                                self.okashiList?.append(okashi)
+                        }
+                        //デバック用に出力する
+                        print("---------------")
+                        self.okashiList?.forEach {print("\($0)")}
+                    }
+                }
+                //取得しているお菓子の数だけタプル配列に格納
+                
+                
+            }catch{
+                //エラー検知
+                print("エラーが発生しました")
+            }
+        })
+        //ダウンロード開始
+        task.resume()
+        
+        
     }
-
+    //レスポンス格納用の構造体全ての要素ごとにnilの可能性もあるので、オプショナル型で宣言
+    struct ItemJson: Codable {
+        //お菓子の名称
+        let name:String?
+        //メーカー
+        let maker:String?
+        //掲載URL
+        let url:URL?
+        //画像URL
+        let image:URL?
+    }
+    //構造体をさらに配列で管理する為の構造体を作る
+    struct ResultJson: Codable {
+        //複数要素nilの可能性もある為オプショナル型で宣言
+        let item:[ItemJson]?
+    }
 }
